0b7d300d619b287d4df466d9b78aca3a3aaae0de
biniam-bin <biniamdnl@gmail.com>
2021-09-10 22:38:18 -0700

    basic setup

---
+++ screens/HomeScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, View, Image } from 'react-native';
+import tw from "tailwind-react-native-classnames";
+const HomeScreen = () => {
+    return (
+        <SafeAreaView style={tw`bg-white-500 h-full`}>
+            <View>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: "contain",
+                    }}
+                    source={{
+                        uri: "https://links.papareact.com/gzs",
+                    }}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
