5307b58d10d0659191ec17f5ea3cc6a77903c43b
biniam-bin <biniamdnl@gmail.com>
2021-09-11 02:04:24 -0700

    first commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView, View, Image } from 'react-native';
 import tw from "tailwind-react-native-classnames";
+import NavOptions from '../components/NavOptions';
+
+
 const HomeScreen = () => {
     return (
-        <SafeAreaView style={tw`bg-white-500 h-full`}>
-            <View>
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
                 <Image
                     style={{
                         width: 100,
@@ -15,6 +18,7 @@ const HomeScreen = () => {
                         uri: "https://links.papareact.com/gzs",
                     }}
                 />
+                <NavOptions/>
             </View>
         </SafeAreaView>
     )
